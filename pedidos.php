4dec52e7f09fcc0ec9dc4d5eed424695910e73d9
Vinicius Luciano de Souza <104027433+ViniLSouza@users.noreply.github.com>
2023-12-04 19:52:34 -0300

    update

--- pedidos.php
+++ pedidos.php
@@ -47,4 +47,17 @@ if (isset($_SESSION['login'])) {
         <?php endif; ?>
     </header>
 
+
+    
+    <footer>
+        <p class="textoF">Kiero Burguer®</p>
+        <p class="textoF dev">Desenvolvido por:</p>
+        <div class="devs">
+            <p class="textoF devs">Gabriel Gevezier</p>
+            <p class="textoF devs">Hemily Batista</p>
+            <p class="textoF devs">Luana Peracini</p>
+            <p class="textoF devs">Vinicius Luciano</p>
+            <p class="textoF devs">Wesley Scolaro</p>
+        </div>
+    </footer>
 </html>
