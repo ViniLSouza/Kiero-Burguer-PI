86586a74c3e4e43a14e479f3f6d53fec4043186c
Vinicius Luciano de Souza <104027433+ViniLSouza@users.noreply.github.com>
2023-12-04 13:20:26 -0300

    updates

--- processar_compra.php
+++ processar_compra.php
@@ -3,6 +3,10 @@ session_start();
 if (isset($_SESSION['login'])) {
     include "conn.php";
 
+    $funcaoConversao = function($valor) {
+        return intval($valor);
+    };
+
     $cliente_id = $_SESSION['login'];
     $carrinhoItens = json_decode(file_get_contents('php://input'), true);
 
@@ -10,7 +14,9 @@ if (isset($_SESSION['login'])) {
 
     $inserir_pedido = $conn->prepare('INSERT INTO pedidos (FKcliente, items) VALUES (:cliente_id, :items)');
     $inserir_pedido->bindParam(':cliente_id', $cliente_id);
-    $inserir_pedido->bindParam(':items', json_encode(intval($carrinhoItens["items"])));
+    $inserir_pedido->bindParam(':items', json_encode(array_map($funcaoConversao, $carrinhoItens["items"])));
+    $array_items = $carrinhoItens["items"];
+    $inserir_pedidoFK_consumivelFK = $conn->prepare('INSERT INTO pedidos_consumiveis (FK_pedido, FK_consumivel) VALUES (:ppedido, :pconsumivel)');
     
     if ($inserir_pedido->execute()) {
         echo json_encode(array("status" => "success", "message" => "Pedido registrado com sucesso!"));
